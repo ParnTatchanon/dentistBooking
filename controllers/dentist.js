2da6fc552a0bddfadd73e83ed6e6698eb48115d2
Tatchanon <pan.tatchanon@gmail.com>
2022-04-21 20:42:32 +0700

    Code Formatter

--- controllers/dentist.js
+++ controllers/dentist.js
@@ -1,131 +1,140 @@
-const { param } = require('express/lib/request');
-const Dentist = require('../models/Dentist');
+const { param } = require("express/lib/request");
+const Dentist = require("../models/Dentist");
 
 //@desc     Get all dentists
 //@route    GET /api/v1/dentists
 //@access   Public
-exports.getDentists= async (req,res,next)=>{
-    let query;
-    //Copy req.quest
-    const reqQuery={...req.query};
+exports.getDentists = async (req, res, next) => {
+  let query;
+  //Copy req.quest
+  const reqQuery = { ...req.query };
 
-    //Fields to exclide
-    const removeFields = ['select',"sort","page","limit"];
+  //Fields to exclide
+  const removeFields = ["select", "sort", "page", "limit"];
 
-    //Loop over remove fields and delete them from reqQuery
-    removeFields.forEach(param => delete reqQuery[param]);
-    console.log(reqQuery);
+  //Loop over remove fields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+  console.log(reqQuery);
 
-    //Create query string
-    let queryStr=JSON.stringify(reqQuery);
-    queryStr=queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match=>`$${match}`);
-    query=Dentist.find(JSON.parse(queryStr)).populate({path:'bookings',select: 'bookingDate'});
+  //Create query string
+  let queryStr = JSON.stringify(reqQuery);
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  query = Dentist.find(JSON.parse(queryStr)).populate({
+    path: "bookings",
+    select: "bookingDate",
+  });
 
-    //Select Fields
-    if(req.query.select){
-        const fields = req.query.select.split(',').join(' ');
-        query=query.select(fields);
+  //Select Fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  //Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("createdAt");
+  }
+
+  //Pagination
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 25;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  try {
+    const total = await Dentist.countDocuments();
+    query = query.skip(startIndex).limit(limit);
+
+    //Execute query
+    const dentists = await query;
+    // console.log(req.query);
+
+    //Pagination result
+    const pagination = {};
+
+    if (endIndex < total) {
+      pagination.next = {
+        page: page + 1,
+        limit,
+      };
     }
 
-    //Sort
-    if(req.query.sort){
-        const sortBy = req.query.sort.split(',').join(' ');
-        query=query.sort(sortBy);
-    }else{
-        query= query.sort('createdAt');
+    if (startIndex > 0) {
+      pagination.prev = {
+        page: page - 1,
+        limit,
+      };
     }
-
-    //Pagination
-    const page = parseInt(req.query.page,10)||1;
-    const limit = parseInt(req.query.limit,10)||25;
-    const startIndex = (page-1)*limit;
-    const endIndex = page*limit;
-   
-
-    try {
-        const total = await Dentist.countDocuments();
-        query =query.skip(startIndex).limit(limit);
-
-        //Execute query
-        const dentists = await query;
-        // console.log(req.query);
-
-        //Pagination result
-        const pagination = {};
-
-        if(endIndex < total){
-            pagination.next={
-                page:page+1,limit
-            }
-        }
-
-        if(startIndex > 0 ){
-            pagination.prev= {
-                page:page-1,limit
-            }
-        }
-        res.status(200).json({success:true, count:dentists.length, data:dentists});
-    } catch(err) {
-        res.status(400).json({success:false});
-    }
+    res
+      .status(200)
+      .json({ success: true, count: dentists.length, data: dentists });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 //@desc     Get single dentist
 //@route    GET /api/v1/dentists/:id
 //@access   Public
-exports.getDentist= async (req,res,next)=>{
-    try {
-        const dentist = await Dentist.findById(req.params.id);
+exports.getDentist = async (req, res, next) => {
+  try {
+    const dentist = await Dentist.findById(req.params.id);
 
-        if(!dentist){
-            return res.status(400).json({success:false});
-        }
-        res.status(200).json({success:true, data:dentist});
-    } catch(err) {
-        res.status(400).json({success:false});
+    if (!dentist) {
+      return res.status(400).json({ success: false });
     }
-}
+    res.status(200).json({ success: true, data: dentist });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@desc     Create new dentist
 //@route    POST /api/v1/dentists
 //@access   Private
-exports.createDentist=async(req,res,next)=>{
-    // console.log(req.body);
-    const dentist = await Dentist.create(req.body);
-    res.status(201).json({success:true, data:dentist});
-}
+exports.createDentist = async (req, res, next) => {
+  // console.log(req.body);
+  const dentist = await Dentist.create(req.body);
+  res.status(201).json({ success: true, data: dentist });
+};
 
 //@desc     Update dentist
 //@route    PUT /api/v1/dentists/:id
 //@access   Private
-exports.updateDentist= async(req,res,next)=>{
-    try {
-        const dentist = await Dentist.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators:true
-        });
+exports.updateDentist = async (req, res, next) => {
+  try {
+    const dentist = await Dentist.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-        if(!dentist){
-            return res.status(400).json({success:false});
-        }
-        res.status(200).json({success:true, data:dentist});
-    } catch(err) {
-        res.status(400).json({success:false});
+    if (!dentist) {
+      return res.status(400).json({ success: false });
     }
-}
+    res.status(200).json({ success: true, data: dentist });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@desc     Delete dentist
 //@route    DELETE /api/v1/dentists/:id
 //@access   Private
-exports.deleteDentist= async (req,res,next)=>{
-    try {
-        const dentist = await Dentist.findById(req.params.id);
+exports.deleteDentist = async (req, res, next) => {
+  try {
+    const dentist = await Dentist.findById(req.params.id);
 
-        if(!dentist){
-            return res.status(400).json({success:false});
-        }
-        dentist.remove();
-        res.status(200).json({success:true, data:{}});
-    } catch(err) {
-        res.status(400).json({success:false});
+    if (!dentist) {
+      return res.status(400).json({ success: false });
     }
-}
+    dentist.remove();
+    res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
--- routes/bookings.js
+++ routes/bookings.js
@@ -1,9 +1,22 @@
-const express = require('express');
-const { getAppointments,getAppointment,addAppointment, updateAppointment , deleteAppointment } = require('../controllers/booking');
-const router = express.Router({mergeParams:true});
-const {protect,authorize} = require ('../middleware/auth');
+const express = require("express");
+const {
+  getAppointments,
+  getAppointment,
+  addAppointment,
+  updateAppointment,
+  deleteAppointment,
+} = require("../controllers/booking");
+const router = express.Router({ mergeParams: true });
+const { protect, authorize } = require("../middleware/auth");
 
-router.route('/').get(protect, getAppointments).post(protect,authorize('admin','user'),addAppointment);
-router.route('/:id').get(protect, getAppointment).put(protect,authorize('admin','user'),updateAppointment).delete(protect,authorize('admin','user'),deleteAppointment);
+router
+  .route("/")
+  .get(protect, getAppointments)
+  .post(protect, authorize("admin", "user"), addAppointment);
+router
+  .route("/:id")
+  .get(protect, getAppointment)
+  .put(protect, authorize("admin", "user"), updateAppointment)
+  .delete(protect, authorize("admin", "user"), deleteAppointment);
 
-module.exports= router;
+module.exports = router;
