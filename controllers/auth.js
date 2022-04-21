20ed38cde064404e061b163a5b245973e4cbf746
Tatchanon <pan.tatchanon@gmail.com>
2022-04-21 11:48:47 +0700

    Add register by specifying

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,13 +8,14 @@ const User = require ('../models/User');
 
 exports.register = async (req,res,next) => {
     try {
-        const {name, email, password, role} = req.body;
+        const {name, email, password, role, telephoneNumber, } = req.body;
         //Create user
         const user = await User.create({
             name,
             email,
             password,
-            role
+            role,
+            telephoneNumber
         });
 
         //Create token
